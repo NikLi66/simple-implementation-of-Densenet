5b4145990dd97e76f9ef36dcf9f6793c06f00c8a
Nik Li <72506160+NikLi66@users.noreply.github.com>
2021-08-07 12:41:10 +0800

    Update TestOnCIFAR10.py

--- TestOnCIFAR10.py
+++ TestOnCIFAR10.py
@@ -9,17 +9,6 @@ import torch.nn.functional as F
 import numpy as np
 import sklearn.metrics as metrics
 from torchvision import datasets, transforms
-#python3
-
-from __future__ import print_function
-import argparse
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-import numpy as np
-import sklearn.metrics as metrics
-from torchvision import datasets, transforms
 import DenseNet
 import sys 
 
