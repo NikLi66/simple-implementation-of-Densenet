8089a86c29b8d0aac626ddc68e452c84c5f0980d
Nik Li <72506160+NikLi66@users.noreply.github.com>
2021-08-07 11:22:57 +0800

    Add files via upload

---
+++ DenseNet.py
@@ -0,0 +1,131 @@
+#python3
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+
+
+def transform(mode):
+    """
+    Apply one of randomly cropping, rotation, changing brightness, changing contrast and horizontal flipping for each input image.
+    """
+    if mode == 'train':
+        ComposedTransform=transforms.Compose([
+            transforms.RandomHorizontalFlip(0.5),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465),(0.2023, 0.1994, 0.2010))
+            ])
+        return ComposedTransform
+    elif mode == 'test':
+        ComposedTransform=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465),(0.2023, 0.1994, 0.2010))
+            ])
+        return ComposedTransform
+
+
+
+class Network(nn.Module):
+    '''
+    k is the grow rate of DenseNet, theta is the compression coefficient
+    dense is a list which contain the depth of each layer
+    theta is the coefficient used in compression/transition layer
+    '''
+    def __init__(self, k=12, dense=[16,16,16], theta=0.5):
+        super().__init__()
+        self.Conv = nn.Conv2d(in_channels=3, out_channels=2*k, kernel_size=(3,3), padding=(1,1), bias=False)
+        self.Pool = nn.MaxPool2d(kernel_size=(2,2), stride=2)
+
+        # Construct Dense Blocks and Transition Layers
+        # Parameter delta is the coefficient of input channels
+        Model = []
+        in_channels = 2*k
+        for i in range(len(dense)):
+          Model.append(self._make_dense_block(in_channels, k, dense[i]))
+          if i < len(dense)-1:
+            in_channels += dense[i]*k
+            out_channels = int(in_channels*theta)
+            Model.append(self._make_transition_layer(in_channels, out_channels))
+            in_channels = out_channels
+        self.model = nn.Sequential(*Model)
+        
+        # Classifier
+        # There is only 1 layer in this classifier.
+        self.classifier = nn.Linear(1*1*(out_channels+dense[-1]*k), 10)
+        self.avg_pool = nn.AvgPool2d(kernel_size=4, stride=4, padding=0)
+
+    def _make_dense_block(self, in_channels, k, number_of_layers):
+        layers = []
+        for l in range(number_of_layers):
+            layers.append(Bottleneck_Layer(in_channels + l*k, k))
+        return nn.Sequential(*layers)
+
+    def _make_transition_layer(self, in_channels, out_channels):
+        modules = []
+        modules.append(Transition_Layer(in_channels, out_channels))
+        return nn.Sequential(*modules)
+
+    def forward(self, x):
+        out = self.Conv(x)
+        out = self.Pool(out)
+        out = self.model(out)
+        out = self.avg_pool(out)
+        out = torch.flatten(out, 1)
+        out = self.classifier(out)
+        return out
+
+
+class Bottleneck_Layer(nn.Module):
+    """
+    Each bottleneck layer consists of BN-ReLU-Conv1*1-BN-ReLU-Conv3*3.
+    The Conv1*1 is to improve computation efficiency.
+    The number of output channel of Conv1*1 is 4*k, which is followed the original paper.
+    """
+    def __init__(self, in_channels, k):
+        super(Bottleneck_Layer, self).__init__()
+        self.composite = nn.Sequential(
+                nn.BatchNorm2d(in_channels),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(in_channels, out_channels=4*k, kernel_size=(1,1)),
+                nn.Dropout(p=0.2),
+                nn.BatchNorm2d(4*k),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(4*k, out_channels=k, kernel_size=(3, 3), padding=(1,1)),
+                nn.Dropout(p=0.2)
+            )
+
+    def forward(self, x):
+        x_1 = self.composite(x)
+        output = torch.cat([x, x_1], 1)         # Concatenation of input and output
+        return output
+
+
+class Transition_Layer(nn.Module):
+    """
+    Each transition layer consists of BN-Conv1*1-AvgPool.
+    This is to improve model compactness.
+    The parameter theta is the compression coefficient 
+    """
+    def __init__(self, in_channels, out_channels):
+        super(Transition_Layer, self).__init__()
+        self.transition = nn.Sequential(
+            nn.BatchNorm2d(num_features=in_channels),
+            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=(1, 1), bias=False),
+            nn.Dropout(p=0.2),
+            nn.AvgPool2d(kernel_size=2, stride=2, padding=0)
+        )
+
+    def forward(self, x):
+        output=self.transition(x)
+        return output
+
+
+
+net = Network()
+lossFunc = nn.CrossEntropyLoss()
+
+batch_size = 64
+epochs = 300
+
---
+++ TestOnCIFAR10.py
@@ -0,0 +1,100 @@
+#python3
+
+from __future__ import print_function
+import argparse
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import numpy as np
+import sklearn.metrics as metrics
+from torchvision import datasets, transforms
+import DenseNet
+import sys 
+
+
+def train(model, device, optimizer, train_loader, epoch, lossF):
+  model.train()
+  total_loss = 0
+  total_images = 0
+  total_correct = 0
+  for batch in train_loader:
+    images, labels= batch
+    images, labels = images.to(device), labels.to(device)
+    
+    pred_y = model(images)
+    loss = lossF(pred_y, labels)
+    
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    output = pred_y.argmax(dim=1)
+
+    total_loss += loss.item()
+    total_images += labels.size(0)
+    total_correct += output.eq(labels).sum().item()
+
+  model_accuracy = total_correct / total_images * 100
+  print('epoch {0} total_correct: {1} loss: {2:.2f} acc: {3:.2f}'.format(
+          epoch,total_correct, total_loss, model_accuracy) )
+        
+def test(model, device, test_loader,lossF):
+  model.eval()
+  total = 0
+  correct = 0
+  with torch.no_grad():
+    for images, labels in test_loader:
+      images, labels = images.to(device), labels.to(device)
+      output = model(images)
+      
+      pred_y = output.argmax(dim=1, keepdim=True)
+      correct += pred_y.eq(labels.view_as(pred_y)).sum().item()
+      total += labels.size(0)
+    
+    model_accuracy = correct / total * 100
+    print('      Accuracy on {0} test images: {1:.2f}%'.format(
+                                total, model_accuracy))
+
+
+def main():
+  loss_values = []
+  acc = []
+
+  # command-line arguments
+  device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+
+  # fetch and load training data
+  trainset = datasets.CIFAR10(root='./data', train=True, download=True, transform=DenseNet.transform('train'))
+  train_loader = torch.utils.data.DataLoader(trainset, batch_size=DenseNet.batch_size, shuffle=False)
+
+  # fetch and load test data
+  testset = datasets.CIFAR10(root='./data', train=False, download=True, transform=DenseNet.transform('test'))
+  test_loader = torch.utils.data.DataLoader(testset, batch_size=DenseNet.batch_size, shuffle=False)
+
+  # choose network architecture
+  net = DenseNet.net.to(device)
+  lossF = DenseNet.lossFunc
+
+  if list(net.parameters()):
+    # use SGD optimizer
+    # training and testing loop
+    for epoch in range(1, DenseNet.epochs + 1):
+      lr = 0.1
+      if epoch >= int(0.5*DenseNet.epochs):
+        lr = 0.01
+      elif epoch >= int(0.75*DenseNet.epochs):
+        lr = 0.001
+      optimizer = optim.SGD(net.parameters(), lr=lr, momentum=0.9, nesterov=True, weight_decay=1e-4)
+      train(net, device, optimizer, train_loader, epoch, lossF)
+      if epoch%10==0:
+        test(net, device, test_loader, lossF)
+
+  torch.save(net.state_dict(), 'CIFAR_Model.pth')
+  print("   Model saved to CIFAR_Model.pth")
+  print(acc)
+  print(loss_values)
+  
+if __name__ == '__main__':
+  main()
